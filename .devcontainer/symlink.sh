25f095bda97b6fa52dbd72f26684843945bcd107
Marius Rieder <marius.rieder@durchmesser.ch>
2024-03-08 12:13:23 +0000

    Create directory for symlink

--- .devcontainer/symlink.sh
+++ .devcontainer/symlink.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 NAME=$(python3 -c 'print(eval(open("package").read())["name"])')
-
+mkdir -p $OMD_ROOT/local/lib/python3/cmk_addons/plugins
 ln -sv $WORKSPACE $OMD_ROOT/local/lib/python3/cmk_addons/plugins/$NAME
